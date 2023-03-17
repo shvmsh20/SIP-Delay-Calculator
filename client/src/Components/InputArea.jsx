9b913d6ea93e27d4c4464801e4dbaef65a04da67
Bhavya <bhavyasharma1401@gmail.com>
2023-03-17 12:03:23 +0530

    refactored code

--- client/src/Components/InputArea.jsx
+++ client/src/Components/InputArea.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import SliderArea from './SliderArea';
+import React from "react";
+import SliderArea from "./SliderArea";
 
-function InputArea(props) {
+function InputArea({
+  monthlyInvestment,
+  setMonthlyInvestment,
+  investmentPeriod,
+  setInvestmentPeriod,
+  rateOfReturn,
+  setRateOfReturn,
+  delay,
+  setDelay,
+}) {
   return (
     <div class="inputArea">
-      <SliderArea index={0} mn={500} mx={10000} steps={50} value={props.monthlyInvestment} setValue={props.changeMonthlyInvestment}/>
-      <SliderArea index={1} mn={1} mx={30} steps={1} value={props.investmentPeriod} setValue={props.changeInvestmentPeriod}/>
-      <SliderArea index={2} mn={1} mx={30} steps={0.1} value={props.rateOfReturn} setValue={props.changeRateOfReturn}/>
-      <SliderArea index={3} mn={1} mx={120} steps={1} value={props.delay} setValue={props.changeDelay}/>
+      <SliderArea
+        index={0}
+        mn={500}
+        mx={10000}
+        steps={50}
+        value={monthlyInvestment}
+        setValue={setMonthlyInvestment}
+      />
+      <SliderArea
+        index={1}
+        mn={1}
+        mx={30}
+        steps={1}
+        value={investmentPeriod}
+        setValue={setInvestmentPeriod}
+      />
+      <SliderArea
+        index={2}
+        mn={1}
+        mx={30}
+        steps={0.1}
+        value={rateOfReturn}
+        setValue={setRateOfReturn}
+      />
+      <SliderArea
+        index={3}
+        mn={1}
+        mx={120}
+        steps={1}
+        value={delay}
+        setValue={setDelay}
+      />
     </div>
-  )
+  );
 }
 
-export default InputArea
+export default InputArea;
